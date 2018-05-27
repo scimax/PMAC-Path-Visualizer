6dd757a1809de3b9097727c555a2b8930b0f964f
Max Kellermeier <max.kellermeier@epfl.ch>
2018-05-27 17:30:15 +0200

    Exporting history improved

--- Laser.py
+++ Laser.py
@@ -15,7 +15,7 @@ class Laser:
         else:
             a = self.home + np.array([x,y,z, 0])
         a[3] = bool(self.status)
-        print("Move:",a)
+#         print("Move:",a)
         self.posHistory.append(a)
         self.pos = a
         return x,y,z
@@ -30,23 +30,15 @@ class Laser:
     def exportHistory(self):
         positions = np.array(self.posHistory)
         ind_split =  np.where( np.abs(np.diff(positions[:,3])) != 0.)[0]
-        ext_ind_split = [0, *ind_split, positions.shape[0]]
-        pos_on_off = [ positions[i:(j+1) ] for i,j in zip( ext_ind_split[:-1], ext_ind_split[1:] ) ]
+        pos_on_off = np.split(positions, ind_split+1)
         np.save("positions.npy", pos_on_off)
-#         np.save("positions.npy", positions)
-        return pos_on_off
+        return np.array(pos_on_off)
     
     def toggle_on_off(self, new_state):
         '''new_state: boolean'''
         if self.status != new_state:
-            print(self.posHistory[-1])
             self.status = new_state
-            self.pos[3]= new_state
-            b = np.copy(self.pos)
-            b[3] = new_state
-            print(self.pos)
-            self.posHistory.append(b)
-            print(self.posHistory[-1])
+            self.move(0,0,0)
     
 class Mode(Enum):
     LINEAR= 0
